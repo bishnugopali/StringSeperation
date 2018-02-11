fdaea151b0f8338f190e1e20ed5177c558dc62f4
Bishnu Gopali <bishnugopali@hotmail.com>
2018-02-11 11:12:31 +0000

    String Seperation

--- StringSeperation/Program.cs
+++ StringSeperation/Program.cs
@@ -10,6 +10,36 @@ namespace StringSeperation
     {
         static void Main(string[] args)
         {
+            string str; /* Declares a string of size 100 */
+            int i;
+            
+            Console.Write("\n\nInput a string :\n");
+            Console.Write("---------------------------------\n");
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+
+            
+            char[] array = str.ToCharArray();
+
+            for (i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+
+       /*   using wjile loop  
+          while (l <= str.Length - 1)
+            {
+                Console.Write("{0} ", str[l]);
+                l++;
+            }**/
+
+
+            Console.Write("\n\n");
+
+            
+
+            Console.ReadLine();
+
         }
     }
 }
